e252778f1f8186c1c0fafb45fb6c34f4dcf0cad2
Bobahan <bobahansem98@icloud.com>
2023-02-26 15:40:41 +0300

    fix home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,40 +1,45 @@
-import { Skeleton } from '@mui/material';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
 import { fetchAllPosts } from '../../store/slices/posts/posts';
 import Post from '../Post/Post';
+import { Preloader } from '../Preloader';
+import styles from './Home.module.scss';
 
 const Home = () => {
   const dispatch = useDispatch();
   const isAuth = useSelector((state) => state.auth.user);
   const { items, isLoading } = useSelector((state) => state.posts.posts);
 
-  const isLoadingPosts = isLoading.status === 'loading';
-
   React.useEffect(() => {
     dispatch(fetchAllPosts());
   }, []);
 
+  if (items.length === 0 || !items) {
+    return <Preloader />;
+  }
+
   return (
-    <>
-      {(isLoadingPosts ? [...Array(5)] : items).map((post, index) =>
-        isLoadingPosts ? (
-          <Post key={index} isLoading={true} />
-        ) : (
+    <div className="container">
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        items.map((post) => (
           <Post
-            _id={post._id}
+            postId={post._id}
             title={post.title}
             body={post.body}
-            img={post.img}
+            img={post.imageUrl ? `http://localhost:4444${post.imageUrl}` : ''}
             key={post._id}
-            user={post.user.fullName}
-            data={post.createdAt}
+            createdAt={post.createdAt}
             viewsCount={post.viewsCount}
-            isOwner={post.user._id === isAuth?._id}
+            user={post.user.name}
+            isOwner={post.user?._id === isAuth?._id}
+            avatarURL={post.user.avatarURL ? `http://localhost:4444${post.user.avatarURL}` : ''}
           />
-        ),
+        ))
       )}
-    </>
+    </div>
   );
 };
 

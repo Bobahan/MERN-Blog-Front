14b53f4eff1d805f20ec4e472332017083a43055
Bobahan <bobahansem98@icloud.com>
2023-02-27 09:40:16 +0300

    reduce to the one scss

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,16 @@
-import { Button, TextField } from '@mui/material';
-import { useForm } from 'react-hook-form';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+import { Button, TextField } from '@mui/material';
+import styles from './Form.module.scss';
 import { fetchAuth } from '../store/slices/auth/auth';
-import styles from './Login.module.scss';
 
 const Login = () => {
   const isAuth = useSelector((state) => state.auth.user);
   const dispatch = useDispatch();
+  const [error, setError] = React.useState(null);
 
   const {
     register,
@@ -15,18 +18,19 @@ const Login = () => {
     formState: { errors, isValid },
   } = useForm({
     defaultValues: {
-      email: 'bobba@mail.ru',
-      password: '123asdsd',
+      email: '',
+      password: '',
     },
     mode: 'all',
   });
 
   const onSubmit = async (values) => {
     let data = await dispatch(fetchAuth(values));
+    if (!data.payload['token']) {
+      setError(data.payload);
+    }
     if ('token' in data.payload) {
       window.localStorage.setItem('token', data.payload.token);
-    } else {
-      alert('Не удалось авторизоваться');
     }
   };
 
@@ -35,29 +39,37 @@ const Login = () => {
   }
 
   return (
-    <div className={styles.register}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1>Login</h1>
-        <TextField
-          className={styles.field}
-          label="E-Mail"
-          type="email"
-          error={Boolean(errors.email?.message)}
-          helperText={errors.email?.message}
-          {...register('email', { required: 'Enter the email' })}
-        />
-        <TextField
-          className={styles.field}
-          label="Password"
-          type="password"
-          {...register('password', { required: 'Enter the password' })}
-          error={Boolean(errors.password?.message)}
-          helperText={errors.password?.message}
-        />
-        <Button disabled={!isValid} type="submit" size="large" variant="contained">
-          Login
-        </Button>
-      </form>
+    <div className="container">
+      <div className={styles.form}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <h1>Login</h1>
+          </div>
+          <TextField
+            className={styles.field}
+            label="E-Mail"
+            type="email"
+            error={Boolean(errors.email?.message)}
+            helperText={errors.email?.message}
+            {...register('email', { required: 'Enter the email' })}
+          />
+          <TextField
+            className={styles.field}
+            label="Password"
+            type="password"
+            {...register('password', { required: 'Enter the password' })}
+            error={Boolean(errors.password?.message)}
+            helperText={errors.password?.message}
+          />
+          {error && <h1 style={{ color: '#d32f2f', textAlign: 'center' }}>{error}</h1>}
+          <div>
+            {' '}
+            <Button disabled={!isValid} type="submit" size="large" variant="contained">
+              Login
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };

6220fd4cc2bc489e5918583df314218ba876d094
Bobahan <bobahansem98@icloud.com>
2023-04-09 17:19:35 +0300

    delete meta http-equiv="Refresh

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store } from './store/store';
-import App from './App';
+import App from './App.js';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
